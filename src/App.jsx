cbc967aac1b145e4ad653f9fdaf9c31b130d76ac
Elias Mashia <mashiaes@gmail.com>
2025-08-21 16:08:12 -1200

    Fix React Router basename for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/portfolio-website">
       <Routes>
         <Route index element={<Home />} />
         <Route path="*" element={<NotFound />} />
